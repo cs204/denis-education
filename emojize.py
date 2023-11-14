f752798914e5b2416f7686a223d9ee768b967a82
test <test>
2023-11-14 21:17:56 +0700

    test

---
+++ emojize.py
@@ -0,0 +1,8 @@
+import emoji
+
+inp = input("Input: ")
+
+if inp == ":1st_place_medal:":
+    print(emoji.emojize("Output: :1st_place_medal:"))
+elif inp == ":money_bag:":
+    print(emoji.emojize("Output: :money_bag:"))
